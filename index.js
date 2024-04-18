6240df829707e413dbb3e8a9b16a3a4ff41aca81
stankajofre <157607276+stankajofre@users.noreply.github.com>
2024-04-18 15:47:43 -0400

    importando express

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-const PORTt = process.env.PORT || 3001
+const PORTt = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
 })
+
+
+
